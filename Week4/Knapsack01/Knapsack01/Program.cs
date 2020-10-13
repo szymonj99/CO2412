e8f3401473d011021832e8386fd4da45c659415c
szymonj99 <szymon.j@hotmail.com>
2020-10-13 15:24:47 +0100

    Add list of items

--- Week4/Knapsack01/Knapsack01/Program.cs
+++ Week4/Knapsack01/Knapsack01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Knapsack01
 {
@@ -8,7 +9,7 @@ namespace Knapsack01
         public int Value { get; set; }
         public String Name { get; set; }
 
-        public Item(ref string name, int weight, int value)
+        public Item(string name, int weight, int value)
         {
             Name = name;
             Weight = weight;
@@ -24,6 +25,26 @@ namespace Knapsack01
     {
         static void Main(string[] args)
         {
+            List<Item> items = new List<Item>();
+
+            Item compass = new Item("Compass", 3, 4);
+            items.Add(compass);
+
+            Item map = new Item("Map", 4, 4);
+            items.Add(map);
+
+            Item drink = new Item("Drink", 5, 7);
+            items.Add(drink);
+
+            Item mintCake = new Item("Kendal Mint Cake", 2, 4);
+            items.Add(mintCake);
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
