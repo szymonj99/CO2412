59ef2bb2cabc7448d8e36d8ca0377bd614dd6d02
szymonj99 <szymon.j@hotmail.com>
2020-10-13 15:17:16 +0100

    Add Item class

---
+++ Week4/Knapsack01/Knapsack01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Knapsack01
+{
+    class Item
+    {
+        public int Weight { get; set; }
+        public int Value { get; set; }
+        public String Name { get; set; }
+
+        public Item(ref string name, int weight, int value)
+        {
+            Name = name;
+            Weight = weight;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\tWeight: " + Weight + "\tValue: " + Value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
