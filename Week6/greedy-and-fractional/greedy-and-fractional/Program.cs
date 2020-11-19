f86376109a38f093fcb4a04e436f075ce199e438
szymonj99 <szymon.j@hotmail.com>
2020-11-19 01:03:59 +0000

    Finish fractional knapsack

--- Week6/greedy-and-fractional/greedy-and-fractional/Program.cs
+++ Week6/greedy-and-fractional/greedy-and-fractional/Program.cs
@@ -23,9 +23,10 @@ namespace greedy_and_fractional
             public override string ToString()
             {
                 return String.Format(
-                    "Weight: {0}\n" +
-                    "Value: {1}\n" +
-                    "Profit: {2:0.00}", Weight, Value, UnitValue);
+                    "{0}\n" +
+                    "Weight: {1}\n" +
+                    "Value: {2}\n" +
+                    "Profit: {3:0.00}", Name, Weight, Value, UnitValue);
             }
 
             private void calculateUnitValue()
@@ -35,6 +36,7 @@ namespace greedy_and_fractional
         }
 
         static readonly string sInputFile = @"ItemList.txt";
+        static readonly int iKnapsackCapacity = 50;
 
         static List<Item> ReadItemsFromFile()
         {
@@ -60,10 +62,46 @@ namespace greedy_and_fractional
         static void Main(string[] args)
         {
             List<Item> items = ReadItemsFromFile();
-            foreach(Item item in items)
+
+            // Sort the List
+            items.Sort(
+                delegate (Item item1, Item item2)
+                {
+                    return item2.UnitValue.CompareTo(item1.UnitValue);
+                });
+
+            foreach (Item item in items)
             {
                 Console.WriteLine(item);
             }
+
+            int KnapsackWeight = 0;
+            List<Item> Knapsack = new List<Item>();
+
+            foreach(Item item in items)
+            {
+                if (KnapsackWeight < (iKnapsackCapacity - item.Weight))
+                {
+                    Knapsack.Add(item);
+                    KnapsackWeight += item.Weight;
+                }
+                else
+                {
+                    Item partialItem = new Item(item.Name, iKnapsackCapacity - KnapsackWeight, (int)(item.Value / (double)(iKnapsackCapacity - KnapsackWeight)));
+                    KnapsackWeight += iKnapsackCapacity - KnapsackWeight;
+                }
+            }
+
+            Console.WriteLine(@"Outputting Knapsack Contents.");
+
+            foreach (Item item in Knapsack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Knapsack items: {0}\n" +
+                "Knapsack Weight: {1}", Knapsack.Count, KnapsackWeight);
+
             Console.ReadLine();
         }
     }
