fdb5f02967a95569f822ec0fac7e826f5788ae87
szymonj99 <szymon.j@hotmail.com>
2020-12-07 04:37:05 +0000

    Start Linked List exercise

---
+++ Week8/linked-lists/linked-lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linked_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
