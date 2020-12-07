fafd9a6fc21649a228be0c242a1c365ca5ce1f41
szymonj99 <szymon.j@hotmail.com>
2020-12-07 04:59:41 +0000

    Complete priority queue dll

--- Week9/priority-queue/priority-queue/Class1.cs
+++ Week9/priority-queue/priority-queue/Class1.cs
@@ -1,8 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace priority_queue
 {
-    public class Class1
+    public class PriorityQueue<T> where T : IComparable<T>
     {
+        private List<T> data;
+
+        public PriorityQueue()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            data.Add(item);
+            int childIndex = data.Count - 1; // starts at the end
+            while (childIndex > 0)
+            {
+                int parentIndex = (childIndex - 1) / 2;
+                if (data[childIndex].CompareTo(data[parentIndex]) >= 0)
+                {
+                    break; // Child item is larger than the parent
+                }
+                T temp = data[childIndex];
+                data[childIndex] = data[parentIndex];
+                data[parentIndex] = temp;
+            }
+        }
+        public T Dequeue()
+        {
+            // assumes pq is not empty; up to calling code
+            int li = data.Count - 1; // last index (before removal)
+            T frontItem = data[0];   // fetch the front
+            data[0] = data[li];
+            data.RemoveAt(li);
+
+            --li; // last index (after removal)
+            int pi = 0; // parent index. start at front of pq
+            while (true)
+            {
+                int ci = pi * 2 + 1; // left child index of parent
+                if (ci > li) break;  // no children so done
+                int rc = ci + 1;     // right child
+                if (rc <= li && data[rc].CompareTo(data[ci]) < 0) // if there is a rc (ci + 1), and it is smaller than left child, use the rc instead
+                    ci = rc;
+                if (data[pi].CompareTo(data[ci]) <= 0) break; // parent is smaller than (or equal to) smallest child so done
+                T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp; // swap parent and child
+                pi = ci;
+            }
+            return frontItem;
+        }
+
+        public T Peek()
+        {
+            T frontItem = data[0];
+            return frontItem;
+        }
+
+        public int Count()
+        {
+            return data.Count;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < data.Count; ++i)
+                s += data[i].ToString() + " ";
+            s += "count = " + data.Count;
+            return s;
+        }
+
+        public bool IsConsistent()
+        {
+            // is the heap property true for all data?
+            if (data.Count == 0) return true;
+            int li = data.Count - 1; // last index
+            for (int pi = 0; pi < data.Count; ++pi) // each parent index
+            {
+                int lci = 2 * pi + 1; // left child index
+                int rci = 2 * pi + 2; // right child index
+
+                if (lci <= li && data[pi].CompareTo(data[lci]) > 0) return false; // if lc exists and it's greater than parent then bad.
+                if (rci <= li && data[pi].CompareTo(data[rci]) > 0) return false; // check the right child too.
+            }
+            return true; // passed all checks
+        }
     }
 }
